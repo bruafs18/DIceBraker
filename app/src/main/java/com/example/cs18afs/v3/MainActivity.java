bdde05dba864b5ac9eaa93bbc0181b11fb5c943d
cs18afs <1818135@brunel.ac.uk>
2019-10-30 23:16:33 +0000

    Dice Roller features

--- app/src/main/java/com/example/cs18afs/v3/MainActivity.java
+++ app/src/main/java/com/example/cs18afs/v3/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -13,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
     public ArrayList<String> al;
 
+    String sPoints = "Points: ";
+    int points;
+    final int NewDIB = 0;
+    final int ERROR = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,22 +35,76 @@ public class MainActivity extends AppCompatActivity {
                 "If you found a magic lantern and a genie gave you three wishes, what would you wish?"
 
         };
-        for(int idx=0;idx<listStdIceBrakers.length;idx++)
-            al.add(listStdIceBrakers[idx]);
+        for(String line : listStdIceBrakers)
+            al.add(line);
+
+        points = 0;
+    }
+
+    int roll_the_dice(int bound)
+    {
+        return new Random().nextInt(bound);
+    }
+
+    void ShowToast(String Message, int length)
+    {
+        Toast.makeText(getApplicationContext(),Message,length).show();
+    }
+
+    int getNumberFromUser()
+    {
+        EditText txtUserNum = (EditText) findViewById(R.id.txtUserNum);
+        int user_num = 0;
+        try{
+            user_num = Integer.parseInt(txtUserNum.getText().toString());
+            if(user_num>6||user_num<1)
+                throw new Exception("Number needs to be between 1 and 6");
+        }
+        catch(Exception ex){
+            ShowToast(ex.getMessage(),Toast.LENGTH_SHORT);
+        }
+
+        return user_num;
+    }
+
+    void sameNumber(){
+        ShowToast("Congratulations",Toast.LENGTH_SHORT);
+        points++;
+        UpdatelblPoints();
+    }
+
+    void UpdatelblPoints()
+    {
+        TextView lblPoints = (TextView)findViewById(R.id.lblPoints);
+        lblPoints.setText(sPoints+points);
+    }
+
+    public void onClickGenNum(View v)
+    {
+        int new_num = roll_the_dice(6)+1;
+
+        int user_num = getNumberFromUser();
+
+        if(user_num == ERROR)
+            return;
+
+        if(user_num==new_num)
+            sameNumber();
+        else
+            ShowToast("Wrong number", Toast.LENGTH_SHORT);
+
     }
 
     public void onClickPlayDIB(View v){
-        Random rand = new Random();
-
-        int index = rand.nextInt(al.size());
-
-        Toast.makeText(getApplicationContext(),al.get(index),Toast.LENGTH_LONG).show();
+        int index = roll_the_dice(al.size());
+        ShowToast(al.get(index),Toast.LENGTH_SHORT);
     }
+
 
     public void btnNewDIB_OnClick(View v){
         Intent intent = new Intent(this, NewDIBRule.class);
         intent.putStringArrayListExtra("Qs", al);
-        startActivityForResult(intent,0);
+        startActivityForResult(intent,NewDIB);
     }
 
     @Override
@@ -53,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         //Retrieve data in the intent
         switch(requestCode)
         {
-            case 0:
+            case NewDIB:
                 al = data.getStringArrayListExtra("Qs");
                 break;
             default:
